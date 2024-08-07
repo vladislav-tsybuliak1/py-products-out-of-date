d17da888b2016e730a70d168a301f7c47610a53f
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-08-07 14:51:55 +0300

    write test for outdated products func

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,27 @@
-# write your code here
+from unittest import mock
+
+from app.main import outdated_products
+
+
+@mock.patch("app.main.datetime")
+def test_outdated_products(mocked_datetime: mock.MagicMock) -> None:
+    mocked_datetime.date.side_effect = [10, 24, 30]
+    mocked_datetime.date.today.return_value = 25
+
+    assert outdated_products([
+        {
+            "name": "salmon",
+            "expiration_date": mocked_datetime.date(),
+            "price": 600
+        },
+        {
+            "name": "chicken",
+            "expiration_date": mocked_datetime.date(),
+            "price": 120
+        },
+        {
+            "name": "duck",
+            "expiration_date": mocked_datetime.date(),
+            "price": 160
+        }
+    ]) == ["salmon", "chicken"]
